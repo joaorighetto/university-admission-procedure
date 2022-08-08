9b703b05e391633aadbad98983eb1a274a96fac5
joaomarcosri <jm_zr@hotmail.com>
2022-08-08 23:06:32 +0100

    reduce repetition

--- university.py
+++ university.py
@@ -1,148 +1,106 @@
 # check if there's still vacancy for given department
-def check_vacancies(department_list):
-    if len(department_list) < department_vacancies:
+def check_vacancies(dep):
+    if len(dep) < department_vacancies:
         return True
     else:
         return False
 
 
 # fill final department list with applicants from sorted list and add them to approved applicants list
-def fill_vacancies(sorted_applicants_list, final_department_list, approved_applicants_list):
-    temp_list = sorted_applicants_list.copy()
-    for j in range(len(temp_list)):
-        if check_vacancies(final_department_list):
-            if temp_list[j] not in approved_applicants_list:
-                final_department_list.append(temp_list[j])
-                removed_item = sorted_applicants_list.pop(sorted_applicants_list.index(temp_list[j]))
-                approved_applicants_list.append(removed_item)
-        else:
-            break
+def fill_vacancies(stage, final, removed):
+    for dep_sorted, final_dep in zip(stage, final):
+        temp_list = dep_sorted.copy()
+        for j in range(len(temp_list)):
+            if check_vacancies(final_dep):
+                if temp_list[j] not in removed:
+                    final_dep.append(temp_list[j])
+                    removed_item = dep_sorted.pop(dep_sorted.index(temp_list[j]))
+                    removed.append(removed_item)
+            else:
+                break
+
+
+# create a sorted list by criteria from a candidate list for each priority stage of the procedure
+def sort(gen_list, dep_list, crit_list, priority):
+    sorted_lists = list()
+    for department, crit in zip(dep_list, crit_list):
+        sorted_lists.append(sorted([x for x in gen_list if x[priority] == department],
+                                   key=crit))
+    return sorted_lists
 
 
 # define sorting criteria for approval
-def bio_criteria(candidate): return max((candidate["chem_score"] + candidate["phys_score"]) / 2, candidate["special_score"])
-def eng_criteria(candidate): return max((candidate["math_score"] + candidate["cs_score"]) / 2, candidate["special_score"])
-def phys_criteria(candidate): return max((candidate["math_score"] + candidate["phys_score"]) / 2, candidate["special_score"])
-def chem_criteria(candidate): return max(candidate["chem_score"], candidate["special_score"])
-def math_criteria(candidate): return max(candidate["math_score"], candidate["special_score"])
+def bio_criteria(candidate): return ((-max((candidate["chem_score"] + candidate["phys_score"]) / 2, candidate["special_score"])),
+                                     candidate["first_name"],
+                                     candidate["last_name"])
 
 
-department_vacancies = int(input())  # select max amount of students allowed in each department
-list_of_applicants = list(dict())
+def eng_criteria(candidate): return ((-max((candidate["math_score"] + candidate["cs_score"]) / 2, candidate["special_score"])),
+                                     candidate["first_name"],
+                                     candidate["last_name"])
+
+
+def phys_criteria(candidate): return ((-max((candidate["math_score"] + candidate["phys_score"]) / 2, candidate["special_score"])),
+                                      candidate["first_name"],
+                                      candidate["last_name"])
+
+
+def chem_criteria(candidate): return ((-max(candidate["chem_score"], candidate["special_score"])),
+                                      candidate["first_name"],
+                                      candidate["last_name"])
+
+
+def math_criteria(candidate): return ((-max(candidate["math_score"], candidate["special_score"])),
+                                      candidate["first_name"],
+                                      candidate["last_name"])
+
+
+department_vacancies = int(input("Enter maximum number of students a department can have: "))  # input max amount of students allowed in each department
+applicants_list = list(dict())
 keys = ["first_name", "last_name", "phys_score", "chem_score", "math_score", "cs_score", "special_score",
         "first_priority", "second_priority", "third_priority"]
 
+# read file and stores applicants' data in a list of dictionaries
 with open(r"applicant_list_7.txt", "r") as file:
     for line in file:
         applicant = list(line.split())
         for i in range(2, 7):  # convert score strings to floats
             applicant[i] = float(applicant[i])
-        list_of_applicants.append(dict(zip(keys, applicant)))
-
-list_of_applicants = sorted(list_of_applicants, key=lambda x: (x["first_name"], x["last_name"]))  # sort applicants list by name in alphabetical order
+        applicants_list.append(dict(zip(keys, applicant)))
 
 approved_applicants = list()
-final_biotech_list = list()
-final_chemistry_list = list()
-final_engineering_list = list()
-final_mathematics_list = list()
-final_physics_list = list()
+bio_final = list()
+chem_final = list()
+eng_final = list()
+math_final = list()
+phys_final = list()
 
+department_list = ["Biotech", "Engineering", "Physics", "Chemistry", "Mathematics"]
+final_lists = [bio_final, eng_final, phys_final, chem_final, math_final]  # create a list of final lists
+criteria_list = [bio_criteria, eng_criteria, phys_criteria, chem_criteria, math_criteria]
 
-bio_first_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["first_priority"] == "Biotech"],
-                                 key=lambda x: -bio_criteria(x))
-eng_first_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["first_priority"] == "Engineering"],
-                                 key=lambda x: -eng_criteria(x))
-phys_first_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["first_priority"] == "Physics"],
-                                  key=lambda x: -phys_criteria(x))
-chem_first_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["first_priority"] == "Chemistry"],
-                                  key=lambda x: -chem_criteria(x))
-math_first_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["first_priority"] == "Mathematics"],
-                                  key=lambda x: -math_criteria(x))
+# first round
+first_round = sort(applicants_list, department_list, criteria_list, "first_priority")  # sort all departments for first priorities
+fill_vacancies(first_round, final_lists, approved_applicants)
 
-fill_vacancies(bio_first_priority_list, final_biotech_list, approved_applicants)
-fill_vacancies(chem_first_priority_list, final_chemistry_list, approved_applicants)
-fill_vacancies(eng_first_priority_list, final_engineering_list, approved_applicants)
-fill_vacancies(math_first_priority_list, final_mathematics_list, approved_applicants)
-fill_vacancies(phys_first_priority_list, final_physics_list, approved_applicants)
+# second round
+second_round = sort(applicants_list, department_list, criteria_list, "second_priority")  # sort all departments for second priorities
+fill_vacancies(second_round, final_lists, approved_applicants)
 
-bio_second_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["second_priority"] == "Biotech"],
-                                  key=lambda x: -bio_criteria(x))
-eng_second_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["second_priority"] == "Engineering"],
-                                  key=lambda x: -eng_criteria(x))
-phys_second_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["second_priority"] == "Physics"],
-                                   key=lambda x: -phys_criteria(x))
-chem_second_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["second_priority"] == "Chemistry"],
-                                   key=lambda x: -chem_criteria(x))
-math_second_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["second_priority"] == "Mathematics"],
-                                   key=lambda x: -math_criteria(x))
+# third round
+third_round = sort(applicants_list, department_list, criteria_list, "third_priority")  # sort all departments for third priorities
+fill_vacancies(third_round, final_lists, approved_applicants)
 
-fill_vacancies(bio_second_priority_list, final_biotech_list, approved_applicants)
-fill_vacancies(chem_second_priority_list, final_chemistry_list, approved_applicants)
-fill_vacancies(eng_second_priority_list, final_engineering_list, approved_applicants)
-fill_vacancies(math_second_priority_list, final_mathematics_list, approved_applicants)
-fill_vacancies(phys_second_priority_list, final_physics_list, approved_applicants)
+# final sort
+final_sorted_list = list()
+for final_list, criteria in zip(final_lists, criteria_list):
+    final_sorted_list.append(sorted(final_list, key=criteria))
 
-bio_third_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["third_priority"] == "Biotech"],
-                                 key=lambda x: -bio_criteria(x))
-eng_third_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["third_priority"] == "Engineering"],
-                                 key=lambda x: -eng_criteria(x))
-phys_third_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["third_priority"] == "Physics"],
-                                  key=lambda x: -phys_criteria(x))
-chem_third_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["third_priority"] == "Chemistry"],
-                                  key=lambda x: -chem_criteria(x))
-math_third_priority_list = sorted([applicant for applicant in list_of_applicants if applicant["third_priority"] == "Mathematics"],
-                                  key=lambda x: -math_criteria(x))
-
-fill_vacancies(bio_third_priority_list, final_biotech_list, approved_applicants)
-fill_vacancies(chem_third_priority_list, final_chemistry_list, approved_applicants)
-fill_vacancies(eng_third_priority_list, final_engineering_list, approved_applicants)
-fill_vacancies(math_third_priority_list, final_mathematics_list, approved_applicants)
-fill_vacancies(phys_third_priority_list, final_physics_list, approved_applicants)
-
-final_biotech_list = sorted(final_biotech_list,
-                            key=lambda x: (-bio_criteria(x), x["first_name"], x["last_name"]))
-final_engineering_list = sorted(final_engineering_list,
-                                key=lambda x: (-eng_criteria(x), x["first_name"], x["last_name"]))
-final_physics_list = sorted(final_physics_list,
-                            key=lambda x: (-phys_criteria(x), x["first_name"], x["last_name"]))
-final_chemistry_list = sorted(final_chemistry_list,
-                              key=lambda x: (-chem_criteria(x), x["first_name"], x["last_name"]))
-final_mathematics_list = sorted(final_mathematics_list,
-                                key=lambda x: (-math_criteria(x), x["first_name"], x["last_name"]))
-
-
-with open("biotech.txt", "w") as file:
-    for applicant in final_biotech_list:
-        final_score = str(bio_criteria(applicant))
-        first_name = applicant["first_name"]
-        last_name = applicant["last_name"]
-        file.write(f"{first_name} {last_name} {final_score}\n")
-
-with open("engineering.txt", "w") as file:
-    for applicant in final_engineering_list:
-        final_score = str(eng_criteria(applicant))
-        first_name = applicant["first_name"]
-        last_name = applicant["last_name"]
-        file.write(f"{first_name} {last_name} {final_score}\n")
-
-with open("chemistry.txt", "w") as file:
-    for applicant in final_chemistry_list:
-        final_score = str(chem_criteria(applicant))
-        first_name = applicant["first_name"]
-        last_name = applicant["last_name"]
-        file.write(f"{first_name} {last_name} {final_score}\n")
-
-with open("mathematics.txt", "w") as file:
-    for applicant in final_mathematics_list:
-        final_score = str(math_criteria(applicant))
-        first_name = applicant["first_name"]
-        last_name = applicant["last_name"]
-        file.write(f"{first_name} {last_name} {final_score}\n")
-
-with open("physics.txt", "w") as file:
-    for applicant in final_physics_list:
-        final_score = str(phys_criteria(applicant))
-        first_name = applicant["first_name"]
-        last_name = applicant["last_name"]
-        file.write(f"{first_name} {last_name} {final_score}\n")
+# write approval results files
+for final_list, criteria, dep_name in zip(final_sorted_list, criteria_list, department_list):
+    with open(f"{dep_name.casefold()}.txt", "w") as file:
+        for applicant in final_list:
+            final_score = str(-criteria(applicant)[0])
+            first_name = applicant["first_name"]
+            last_name = applicant["last_name"]
+            file.write(f"{first_name} {last_name} {final_score}\n")
